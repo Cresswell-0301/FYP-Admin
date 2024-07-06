3576b9d17e42b5017f4ca72f5b3cc18239a58969
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2024-07-06 21:01:33 +0800

    fixed upload image error

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,48 +1,61 @@
-import multiparty from 'multiparty';
-import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
-import fs from 'fs';
-import mime from 'mime-types';
-import {mongooseConnect} from "@/lib/mongoose";
-import {isAdminRequest} from "@/pages/api/auth/[...nextauth]";
-const bucketName = 'looi-next-ecommerce';
+import multiparty from "multiparty";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
+import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "@/pages/api/auth/[...nextauth]";
 
-export default async function handle(req,res) {
+const bucketName = "fyp-admin-storage";
+
+export default async function handle(req, res) {
   await mongooseConnect();
-  await isAdminRequest(req,res);
+  await isAdminRequest(req, res);
 
   const form = new multiparty.Form();
-  const {fields,files} = await new Promise((resolve,reject) => {
+  const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       if (err) reject(err);
-      resolve({fields,files});
+      resolve({ fields, files });
     });
   });
-  console.log('length:', files.file.length);
+
+  console.log("length:", files.file.length);
+
   const client = new S3Client({
-    region: 'ap-southeast-2',
+    region: "ap-southeast-2",
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     },
   });
+
   const links = [];
   const filesArray = Array.isArray(files.file) ? files.file : [files.file];
   for (const file of filesArray) {
-    const ext = file.originalFilename.split('.').pop();
-    const newFilename = Date.now() + '.' + ext;
-    await client.send(new PutObjectCommand({
-      Bucket: bucketName,
-      Key: newFilename,
-      Body: fs.readFileSync(file.path),
-      ACL: 'public-read',
-      ContentType: mime.lookup(file.path),
-    }));
-    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
-    links.push(link);
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+
+    try {
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: newFilename,
+          Body: fs.readFileSync(file.path),
+          ContentType: mime.lookup(file.path),
+        })
+      );
+
+      const link = `https://${bucketName}.s3.ap-southeast-2.amazonaws.com/${newFilename}`;
+      links.push(link);
+    } catch (error) {
+      console.error("Error uploading file to S3:", error);
+      return res.status(500).json({ error: "Error uploading file to S3" });
+    }
   }
-  return res.json({links});
+
+  return res.json({ links });
 }
 
 export const config = {
-  api: {bodyParser: false},
+  api: { bodyParser: false },
 };

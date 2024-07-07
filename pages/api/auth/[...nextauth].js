090a9d01886832484be52c75a57cd5751960a327
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2024-07-07 11:00:14 +0800

    123

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -26,14 +26,6 @@ export const authOptions = {
   },
 
   secret: process.env.NEXTAUTH_SECRET,
-
-  pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
-    error: "/auth/error", // Error code passed in query string as ?error=
-    verifyRequest: "/auth/verify-request", // (used for check email message)
-    newUser: null, // If set, new users will be directed here on first sign in
-  },
 };
 
 export default NextAuth(authOptions);
